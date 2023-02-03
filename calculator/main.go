dbd6b343d22c5beb4cf63878e491b6c1c897924e
shekars <shekar.singh@amdocs.com>
2023-02-03 23:01:40 +0530

    calculator creation

--- calculator/main.go
+++ calculator/main.go
@@ -1,7 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
 
 func main() {
 	fmt.Println("A simple calculator")
+	reader := bufio.NewReader(os.Stdin)
+	fmt.Printf("value 1:")
+	input1, _ := reader.ReadString('\n')
+	fmt.Printf("value 2:")
+	input2, _ := reader.ReadString('\n')
+	aFloatValue1, err := strconv.ParseFloat(strings.TrimSpace(input1), 64)
+	aFloatValue2, err := strconv.ParseFloat(strings.TrimSpace(input2), 64)
+
+	if err != nil {
+		panic(err)
+	}
+
+	aFloatSum := aFloatValue1 + aFloatValue2
+	fmt.Printf("Sum : %.2f \n ", aFloatSum)
+
 }
