d53ebe4c480bdce587c0fe598ce17415cfdb6e84
shekars <shekar.singh@amdocs.com>
2023-02-03 22:23:55 +0530

    date and time

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,14 @@ package main
 
 import (
 	"fmt"
+	"time"
 )
 
 func main() {
 
-	fmt.Println("Dates and times")
+	n := time.Now()
+	fmt.Println("I recorded this video at", n)
+	t := time.Date(2000, time.August, 14, 8, 30, 0, 0, time.Local)
+	fmt.Println("my birthday is at :", t)
 
 }
