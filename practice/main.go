c3074932ecc0d5a8679653eef9eb66de32ab23a2
shekars <shekar.singh@amdocs.com>
2023-02-04 20:43:41 +0530

    maps - deleting ,looping & appending

--- practice/main.go
+++ practice/main.go
@@ -2,8 +2,38 @@ package main
 
 import (
 	"fmt"
+	"sort"
 )
 
 func main() {
 	fmt.Println("Maps")
+	states := make(map[string]string)
+	states["MH"] = "Maharastra"
+	states["KL"] = "Kerala"
+	states["TN"] = "Tamil Nadu"
+
+	fmt.Println("states :", states)
+
+	fmt.Println("--Deleting a state ---")
+	delete(states, "TN")
+	fmt.Println("states :", states)
+	states["JK"] = "Jammu & Kashmir"
+
+	for k, v := range states {
+		fmt.Printf("%v : %v\n", k, v)
+	}
+
+	stateKeys := make([]string, len(states))
+	i := 0
+	for k := range states {
+		stateKeys[i] = k
+		i++
+	}
+	fmt.Println(stateKeys)
+	sort.Strings(stateKeys)
+	fmt.Println(stateKeys)
+
+	for i := range stateKeys {
+		fmt.Println(states[stateKeys[i]])
+	}
 }
