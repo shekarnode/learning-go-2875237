ecbed10ee139c9ef12e0f2dccfdc3b544ba9d727
shekars <shekar.singh@amdocs.com>
2023-02-04 21:27:37 +0530

    if else condition

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,20 @@ import (
 )
 
 func main() {
-	fmt.Println("Conditional logic")
+	theAnswer := 42
+	if theAnswer < 0 {
+		fmt.Println("Less than Zero")
+	} else if theAnswer == 0 {
+		fmt.Println("Equals to zero")
+	} else {
+		fmt.Println("Greater than zero")
+	}
+
+	if x := -42; x < 0 {
+		fmt.Println("Less than Zero")
+	} else if x == 0 {
+		fmt.Println("Equals to zero")
+	} else {
+		fmt.Println("Greater than zero")
+	}
 }
