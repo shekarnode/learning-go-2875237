f343719855fdf38453eb6405252ec24f94f10459
shekars <shekar.singh@amdocs.com>
2023-02-04 15:40:34 +0530

    arrays and length of arrays

--- practice/main.go
+++ practice/main.go
@@ -1,9 +1,19 @@
 package main
 
-import (
-	"fmt"
-)
+import "fmt"
 
 func main() {
-	fmt.Println("Arrays")
+	var colors [2]string
+	colors[0] = "red"
+	colors[1] = "pink"
+
+	fmt.Println("values of colors array is :", colors)
+	fmt.Println("value of 0th index in colors array is: ", colors[0])
+	fmt.Println("length of colors array is :", len(colors))
+	fmt.Println("---------------------------------------------")
+	//single line declaration and initialization of array
+	number := [3]int{1, 2, 3}
+	fmt.Println("values in number array is :", number)
+	fmt.Println("length of number array is :", len(number))
+
 }
