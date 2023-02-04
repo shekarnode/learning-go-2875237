1a92b0cb1bb89f8932e09e043794823855775645
shekars <shekar.singh@amdocs.com>
2023-02-04 21:11:49 +0530

    structs - debug & printing

--- practice/main.go
+++ practice/main.go
@@ -6,4 +6,13 @@ import (
 
 func main() {
 	fmt.Println("Structs")
+	shekar := Employee{"Shekar Singh", 1566}
+	fmt.Println(shekar)
+	fmt.Printf("%+v\n", shekar)
+	fmt.Printf("Name: %v\nEmployee Id : %v\n", shekar.Name, shekar.EmpId)
+}
+
+type Employee struct {
+	Name  string
+	EmpId int
 }
