0ed6eb78ab732e19ef31b608bdd7ddb01ab5f7a8
shekars <shekar.singh@amdocs.com>
2023-02-04 15:11:42 +0530

    pointers and changing values through pointer

--- practice/main.go
+++ practice/main.go
@@ -5,5 +5,24 @@ import (
 )
 
 func main() {
-	fmt.Println("Pointers")
+	value := 24.5
+	var p = &value
+	fmt.Println("value of p is :", *p)
+
+	value2 := "shekar"
+	pointer1 := &value2
+	fmt.Println("address of value 2: ", &value2)
+	fmt.Println("value of value2 : ", value2)
+	fmt.Println("address of pointer1 : ", pointer1)
+	fmt.Println("value of pointer1 : ", *pointer1)
+
+	//changing value through pointer
+
+	value3 := "singh"
+	fmt.Println("value of value3 : ", value3)
+	pointer2 := &value3
+	fmt.Println("value of pointer2 : ", *pointer2)
+	*pointer2 = "shekar singh"
+	fmt.Println("new value of value3 :", value3)
+
 }
