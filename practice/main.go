09bc26da0514c5743ee37977c21fa4b1aff20ee9
shekars <shekar.singh@amdocs.com>
2023-02-04 21:37:20 +0530

    switch cases

--- practice/main.go
+++ practice/main.go
@@ -8,6 +8,16 @@ import (
 
 func main() {
 	rand.Seed(time.Now().Unix())
-	dow := rand.Intn(7) + 1
-	fmt.Println("Day", dow)
+	// dow := rand.Intn(7) + 1
+	// fmt.Println("Day", dow)
+
+	switch dow := rand.Intn(7) + 1; dow {
+	case 1:
+		fmt.Println("it's sunday !!")
+		// fallthrough
+	case 2:
+		fmt.Println("it's monday !!")
+	default:
+		fmt.Println("it's some other day!!")
+	}
 }
