249c78e87d98684e269999d4e576730aedece6ba
shekars <shekar.singh@amdocs.com>
2023-02-05 18:54:01 +0530

    write and read file

--- calculator/main.go
+++ calculator/main.go
@@ -2,8 +2,31 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"io/ioutil"
+	"os"
 )
 
 func main() {
-	fmt.Println("Files")
+	content := "Hello from Go !!!"
+	file, err := os.Create("./newFile.txt")
+	checkError(err)
+	length, err := io.WriteString(file, content)
+	checkError(err)
+	fmt.Printf("wrote a file with %v character \n", length)
+	defer file.Close()
+	defer readFile("./newFile.txt")
+
+}
+
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
+}
+
+func readFile(fileName string) {
+	data, err := ioutil.ReadFile(fileName)
+	checkError(err)
+	fmt.Println("Text read from file :", string(data))
 }
