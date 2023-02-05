62f36c71a7bdfca05bd983cfd66554e84c5e22c7
shekars <shekar.singh@amdocs.com>
2023-02-05 19:13:32 +0530

    read json from web

--- practice/main.go
+++ practice/main.go
@@ -2,10 +2,27 @@ package main
 
 import (
 	"fmt"
+	"io/ioutil"
+	"net/http"
 )
 
 const url = "http://services.explorecalifornia.org/json/tours.php"
 
 func main() {
 	fmt.Println("Network requests")
+	resp, err := http.Get(url)
+	checkError(err)
+
+	defer resp.Body.Close()
+	byte, err := ioutil.ReadAll(resp.Body)
+	checkError(err)
+
+	content := string(byte)
+	fmt.Println(content)
+
+}
+func checkError(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
