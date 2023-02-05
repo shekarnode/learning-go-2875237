608d0b5fed1e27bafd85d4717f554a9ae0ee0de3
shekars <shekar.singh@amdocs.com>
2023-02-05 17:04:25 +0530

    function , sing and multiple return via function

--- practice/main.go
+++ practice/main.go
@@ -6,4 +6,27 @@ import (
 
 func main() {
 	fmt.Println("Functions")
+	doSomething()
+	sum := addValues(4, 5)
+	fmt.Println("sum is :", sum)
+
+	multiSum, multiCount := multiValues(5, 2, 6, 7)
+	fmt.Println("multisum value is :", multiSum)
+	fmt.Println("multisum count is : ", multiCount)
+}
+
+func doSomething() {
+	fmt.Println("Doing something")
+}
+
+func addValues(value1 int, value2 int) int {
+	return value1 + value2
+}
+
+func multiValues(values ...int) (int, int) {
+	total := 0
+	for _, v := range values {
+		total += v
+	}
+	return total, len(values)
 }
