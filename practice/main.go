d1715bb17855db33cc1a624ea17b5fe7702056ed
shekars <shekar.singh@amdocs.com>
2023-02-05 16:42:00 +0530

    for loop , for each loop and goto label

--- practice/main.go
+++ practice/main.go
@@ -7,4 +7,29 @@ import (
 func main() {
 	colors := []string{"Red", "Green", "Blue"}
 	fmt.Println(colors)
+
+	for i := 0; i < len(colors); i++ {
+		fmt.Println(colors[i])
+	}
+	fmt.Println("----------------------------------------------------------------")
+	for i := range colors {
+		fmt.Println(colors[i])
+	}
+
+	for i, color := range colors {
+		fmt.Printf("%v : %v \n", i, color)
+	}
+
+	//labels
+	sum := 1
+	for sum < 1000 {
+		sum += sum
+		fmt.Println("sum :", sum)
+		if sum > 200 {
+			goto theEnd
+		}
+	}
+
+theEnd:
+	fmt.Println("End of Program")
 }
