a93f05d50963bc6d6536b60fc62af12033cacede
shekars <shekar.singh@amdocs.com>
2023-02-05 17:35:14 +0530

    csutom type function

--- practice/main.go
+++ practice/main.go
@@ -5,14 +5,30 @@ import (
 )
 
 func main() {
-	poodle := Dog{"Poodle", 10}
+	poodle := Dog{"Poodle", 10, "woof!!"}
+	poodleptr := &poodle
 	fmt.Println(poodle)
 	fmt.Printf("%+v\n", poodle)
 	fmt.Printf("Breed: %v\nWeight: %v\n", poodle.Breed, poodle.Weight)
+	poodle.speak()
+	poodle.Sound = "Arf !!"
+	poodle.speak()
+	poodleptr.speakThreeTimes()
+	poodleptr.speakThreeTimes()
 }
 
 // Dog is a struct
 type Dog struct {
 	Breed  string
 	Weight int
+	Sound  string
+}
+
+func (d Dog) speak() {
+	fmt.Printf(d.Sound)
+}
+
+func (d Dog) speakThreeTimes() {
+	d.Sound = fmt.Sprintf("%v %v %v", d.Sound, d.Sound, d.Sound)
+	fmt.Println(d.Sound)
 }
